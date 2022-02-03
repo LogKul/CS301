3c6ad6c9bb97448b8ad8acb79e5e2e9384720d67
LogKul <kule7126@bears.unco.edu>
2022-02-03 15:37:13 -0700

    saving so it stops bugging me

--- listdictruntimes.py
+++ listdictruntimes.py
@@ -3,13 +3,13 @@ import time
 LIST_LENGTHS = [100, 10000, 1000000, 100000000, 10000000000]
 INTERVAL = 10
 
-def test_runtime(func_name, f, arg, lengths, intvl, f_out):
-    f_out.write(func_name + ",")
-    
+
+def test_runtime(f, arg, lengths, intvl, f_out):
+
     for list_length in lengths:
         # --Generate list--
         n_length_list = [1] * list_length
-        
+
         # --Run function intvl times and take average--
         avg_runtime = 0
         for i in range(intvl):
@@ -18,12 +18,13 @@ def test_runtime(func_name, f, arg, lengths, intvl, f_out):
             end_time = time.time()
             avg_runtime += (end_time - start_time)
         avg_runtime = avg_runtime / intvl
-        
+
         # --Write average runtime given specific list length--
         f_out.write(str(avg_runtime) + ",")
-    
+
     # --Make sure to add newline after all relevant info is added to csv file--
     f_out.write("\n")
+
 
 f_out = open("lmao.csv", "w+")
 
