d140158cb0f1ac0adefa523d3db8cb65b95c74d5
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-02-03 07:32:28 -0700

    Updated a bit

--- listdictruntimes.py
+++ listdictruntimes.py
@@ -3,21 +3,32 @@ import time
 LIST_LENGTHS = [100, 10000, 1000000, 100000000, 10000000000]
 INTERVAL = 10
 
-def test_runtime(f, arg, lengths, intvl, f_out):
-    avg_runtime = 0
-
-    for i in range(intvl):
-        start_time = time.time()
-        f(arg)
-        end_time = time.time()
-        avg_runtime += (end_time - start_time)
-    avg_runtime = avg_runtime / intvl
-    f_out.write(str(avg_runtime) + ",")
+def test_runtime(func_name, f, arg, lengths, intvl, f_out):
+    f_out.write(func_name + ",")
+    
+    for list_length in lengths:
+        # --Generate list--
+        n_length_list = [1] * list_length
+        
+        # --Run function intvl times and take average--
+        avg_runtime = 0
+        for i in range(intvl):
+            start_time = time.time()
+            f(arg, list_length)
+            end_time = time.time()
+            avg_runtime += (end_time - start_time)
+        avg_runtime = avg_runtime / intvl
+        
+        # --Write average runtime given specific list length--
+        f_out.write(str(avg_runtime) + ",")
+    
+    # --Make sure to add newline after all relevant info is added to csv file--
+    f_out.write("\n")
 
 f_out = open("lmao.csv", "w+")
 
 # --CODE BELOW--
 
 
-# --CLOSE FILE
+# --CLOSE FILE--
 f_out.close()
