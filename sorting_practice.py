56249a7cb61b8c68badbb04a1a4629b44ac29bb2
LogKul <kule7126@bears.unco.edu>
2022-03-24 16:00:48 -0600

    real

--- sorting_practice.py
+++ sorting_practice.py
@@ -2,7 +2,7 @@
 a = [26, 54, 93, 17, 77, 31, 44, 55, 20]
 
 # Insertion sort
-b = a
+b = [26, 54, 93, 17, 77, 31, 44, 55, 20]
 for i in range(1, len(b)):
     j = i
     while (b[j] < b[j-1]) & (j > 0):
@@ -13,8 +13,8 @@ for i in range(1, len(b)):
 print("Insertion Sort:", b)
 
 # Bubble Sort
-c = a
-for i in range(1, len(c)):
+c = [26, 54, 93, 17, 77, 31, 44, 55, 20]
+for i in range(0, len(c)):
     for j in range(1, len(c) - i):
         if (c[j] < c[j-1]):
             temp = c[j-1]
@@ -23,7 +23,7 @@ for i in range(1, len(c)):
 print("Bubble Sort:   ", c)
 
 # Selection Sort
-d = a
+d = [26, 54, 93, 17, 77, 31, 44, 55, 20]
 for i in range(len(d)):
     min = 0
     for j in range(len(d) - i):
@@ -34,4 +34,41 @@ for i in range(len(d)):
 print("Selection Sort:", d)
 
 # Merge Sort
-e = a
+e = [26, 54, 93, 17, 77, 31, 44, 55, 20]
+
+
+def mergeSort(li):
+    if len(li) > 1:
+        mid = len(li) // 2
+        L = li[:mid]
+        R = li[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                li[k] = L[i]
+                i += 1
+            else:
+                li[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            li[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            li[k] = R[j]
+            j += 1
+            k += 1
+
+
+mergeSort(e)
+print("Merge Sort:    ", e)
